e4567e4ba19d96405c58b96cf4e4a4fe45c83fc0
Tedros <tedtfu@gmail.com>
2025-03-13 16:55:28 -0400

    init chatbot

---
+++ FAQClassifier.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+class FAQClassifier(nn.Module):
+    def __init__(self, input_dim=300, hidden_dim=256, output_dim=1000):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(0.3)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
