e2851b2e1b7c3341a435dc232659162d789097f9
Tedros <tedtfu@gmail.com>
2025-04-10 20:32:14 -0400

    updated project structure

--- train.py
+++
@@ -1,9 +0,0 @@
-import json
-from sklearn.model_selection import train_test_split
-
-with open("data/faqs.json", "r") as f:
-    faqs = json.load(f)
-
-X = [get_embedding(preprocess_text(faq["question"])) for faq in faqs]
-y = [faq["id"] for faq in faqs]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
