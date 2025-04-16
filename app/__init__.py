89d34a9f0f5b73b699b43c545c4cedf4a62d359a
Tedros <tedtfu@gmail.com>
2025-04-15 20:29:30 -0400

    loaded faqs.json file once during app startup

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from config import Config
-
+from app.data.faq_manager import load_faqs
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.faqs = load_faqs()
     from app import routes
     app.register_blueprint(routes.bp)
     return app
