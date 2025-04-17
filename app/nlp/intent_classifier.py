1e1e6e9fc0469afa9c821462139a930b4291092e
Tedros <tedtfu@gmail.com>
2025-04-17 15:10:49 -0400

    sentence transformer integration

--- app/nlp/intent_classifier.py
+++ app/nlp/intent_classifier.py
@@ -1,10 +1,14 @@
 import pickle
-from sklearn.pipeline import make_pipeline
+from sentence_transformers import SentenceTransformer
+from config import Config
+
+model = SentenceTransformer(Config.SENTENCE_TRANSFORMER_MODEL)
 
 def load_classifier():
     with open("models/classifier.pkl", "rb") as f:
         return pickle.load(f)
 
 def predict_intent(query):
+    query_embedding = model.encode(query)
     classifier = load_classifier()
-    return classifier.predict([query])[0]
+    return classifier.predict([query_embedding])[0]
--- app/nlp/similarity_matcher.py
+++ app/nlp/similarity_matcher.py
@@ -1,13 +1,13 @@
-import spacy
+from sentence_transformers import SentenceTransformer, util
 from config import Config
 
-nlp = spacy.load(Config.SPACY_MODEL)
-
+model = SentenceTransformer(Config.SENTENCE_TRANSFORMER_MODEL)
 def match_faq(query, intent, faqs):
-    query_doc = nlp(query)
-    intent_faqs = [f for f in faqs if f["intent"] == intent]
-    if not intent_faqs:
-        return "Sorry, I don’t know that yet!"
-    best_match = max(intent_faqs, key=lambda x: query_doc.similarity(nlp(x["question"])))
-    score = query_doc.similarity(nlp(best_match["question"]))
-    return best_match["answer"] if score > 0.85 else "Sorry, I don’t know that yet!"
+    query_embedding = model.encode(query, convert_to_tensor=True)
+    faq_embeddings = [model.encode(faq["question"], convert_to_tensor=True) for faq in faqs]
+    similarities = [util.cos_sim(query_embedding, faq_emb).item() for faq_emb in faq_embeddings]
+    max_idx = similarities.index(max(similarities))
+
+    if similarities[max_idx] > 0.80:  
+        return faqs[max_idx]["answer"]
+    return "Sorry, I don’t know!"
