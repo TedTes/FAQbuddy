8b4216d2873ce6abb57a33fa5af6199d5356f18c
Tedros <tedtfu@gmail.com>
2025-04-17 15:10:02 -0400

    trained model using sentence trasformer

--- scripts/train_classifier.py
+++ scripts/train_classifier.py
@@ -1,8 +1,9 @@
-import json
-import pickle
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sentence_transformers import SentenceTransformer
 from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import make_pipeline
+import pickle, json
+from config import Config
+
+model = SentenceTransformer(Config.SENTENCE_TRANSFORMER_MODEL)
 
 def train_classifier():
     # Load training data
@@ -14,11 +15,9 @@ def train_classifier():
     intents = [item["intent"] for item in data]
     
     # Create and train pipeline
-    classifier = make_pipeline(
-        TfidfVectorizer(),
-        LogisticRegression()
-    )
-    classifier.fit(queries, intents)
+    query_embeddings = model.encode(queries)
+    classifier = LogisticRegression()
+    classifier.fit(query_embeddings, intents)
     
     # Save to classifier.pkl
     with open("models/classifier.pkl", "wb") as f:
