abee125a80106b14281b1ad8de36086d638ec45c
Tedros <tedtfu@gmail.com>
2025-04-19 09:53:53 -0400

    add cors

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from app.routes import register_routes
 from flask_jwt_extended import  JWTManager
+from flask_cors import CORS
 
 jwt = JWTManager()
 
@@ -9,5 +10,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     jwt.init_app(app)
+    CORS(app, supports_credentials=True)
     register_routes(app)
     return app
