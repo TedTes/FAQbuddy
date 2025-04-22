d7354445d104bb886a8058cf1035c8afd4d18c85
Tedros <tedtfu@gmail.com>
2025-04-22 12:13:01 -0400

    vite config imprvmnt

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,16 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base:'/',
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
-    }
+    },
+    historyApiFallback: true
   },
   build: {
-    outDir: resolve(__dirname, '../app/static'), 
+    outDir: resolve(__dirname, '../app/static/'), 
+    assetsDir: 'assets',
     emptyOutDir: true, // Clears old build files before output
     rollupOptions: {
       input: {
